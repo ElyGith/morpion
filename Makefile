0b1fc0c1e0f30c9c48282a5dffc5f928b053defe
Ely <ely.wandeu@tu.univ-lyon1.fr>
2025-04-08 18:19:04 +0200

    début du morpion, affichage des croix

---
+++ Makefile
@@ -0,0 +1,11 @@
+
+front:
+	cd frontend && npm run dev
+
+run :
+	npm run dev
+
+back:
+	cd backend && npm run dev
+
+
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,11 +3,74 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+console.log('Square component 1');
+type SquareValue = 'O'|'X'|null|number;
+type SquareProps = {
+  value: SquareValue;
+};
+/* const Square = ({ value }: SquareProps) => {
+
+syntaxe avec un paramètre
+
+équivalent a function Square {
+}
+*/
+
+const Square = () => {
+
+const [value,setValue]  = useState<SquareValue>(null);
+
+  function handleClick() {
+    console.log('You clicked !');
+    setValue('X');
+
+  }
+
+  return (
+    <button onClick={handleClick} className="square">
+      {value}
+    </button>
+  );
+};
+
+
 function App() {
+  console.log('Square component 2');
+
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <div className="todo-list" >
+<div className='header'> </div>
+<div className='body'> 
+          <div className='row'>
+             <Square />
+            <Square  />
+            <Square  />
+
+          </div>
+          <div className='row'>
+            <Square />
+            <Square  />
+            <Square />
+          </div>
+           <div className='row'>
+            <Square/>
+            <Square />
+            <Square  />
+  </div>
+
+
+
+
+  
+</div>
+
+    </div> {/* syntaxe commentaire*/}
+
+
+
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
