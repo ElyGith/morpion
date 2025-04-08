265dfa8906f1ed2c6877c7ca083040fa0c6f4aaa
Ely <ely.wandeu@tu.univ-lyon1.fr>
2025-04-08 15:04:57 +0200

    bases du frontend et backend avec react et typescript

---
+++ backend/src/app.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello from TypeScript backend 👋')
+})
+
+export default app
